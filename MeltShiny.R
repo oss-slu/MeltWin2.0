ff9329ebd505f9ee7ae426a275d2b887af2c637e
adhampton110 <anthony.hampton@slu.edu>
2022-10-11 16:30:10 -0500

    reactiveness to server

--- MeltShiny.R
+++ MeltShiny.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
-library(MeltR)
+#library(MeltR)
 library(shiny)
 
 # Define UI ----
@@ -29,38 +29,45 @@ ui <- navbarPage("MeltShiny",
 )      
 
 server <- function(input,output){
+  #Reactive list variable 
+  values <- reactiveValues(masterFrame=NULL,up=NULL)
   #Upload Project File
+  upload <- observeEvent(eventExpr =input$inputFile,
+                         handlerExpr = {
+                           req(input$inputFile)
+                           #Declaring variables
+                           pathlengths <- c(unlist(strsplit(input$pathlengths,",")))
+                           req(input$inputFile)
+                           fileName = input$inputFile$datapath
+                           cd <- read.csv(file = fileName,header=FALSE)
+                           df <- cd %>% select_if(~ !any(is.na(.)))
+                           #Creating temporary frame to store sample data
+                           columns <- c("Sample", "Pathlength", "Temperature", "Absorbance")
+                           tempFrame <- data.frame(matrix(nrow = 0, ncol = 4))
+                           colnames(tempFrame) <- columns
+                           readings <- ncol(df)
+                           #Loop that appends sample data 
+                           counter <- 1
+                           for (x in 2:readings){
+                             col <- df[x]
+                             sample<-rep(c(counter),times=nrow(df[x]))
+                             pathlength<-rep(c(pathlengths[counter]),times=nrow(df[x]))
+                             col <- df[x]
+                             t <- data.frame(sample,pathlength,df[1],df[x])
+                             names(t) <- names(tempFrame)
+                             tempFrame <- rbind(tempFrame, t)
+                             counter <- counter + 1
+                           }
+                           values$numReadings <- counter-1
+                           values$masterFrame <- tempFrame
+                           values$up <- 1
+                           #print(values$up)
+                         }
+  )
   output$contents <- renderTable({
-    req(input$inputFile)
-    if(exists('mainFrame') == FALSE){
-      mainFrame <- data.frame(matrix(nrow = 0, ncol = 4))
-    }
-    #Declaring variables
-    pathlengths <- c(unlist(strsplit(input$pathlengths,",")))
-    fileName = input$inputFile$datapath
-    cd <- read.csv(file = fileName,header=FALSE)
-    df <- cd %>% select_if(~ !any(is.na(.)))
-    #Creating temporary frame to store sample data
-    columns <- c("Sample", "Pathlength", "Temperature", "Absorbance")
-    tempFrame <- data.frame(matrix(nrow = 0, ncol = 4))
-    colnames(tempFrame) <- columns
-    readings <- ncol(df)
-    #Loop that appends sample data 
-    counter <- 1
-    for (x in 2:readings){
-      col <- df[x]
-      sample<-rep(c(counter),times=nrow(df[x]))
-      pathlength<-rep(c(pathlengths[counter]),times=nrow(df[x]))
-      col <- df[x]
-      t <- data.frame(sample,pathlength,df[1],df[x])
-      names(t) <- names(tempFrame)
-      tempFrame <- rbind(tempFrame, t)
-      counter <- counter + 1
-    }
-    mainFrame <- rbind(mainFrame,tempFrame)
-    return(mainFrame)
-  })
+    return(values$masterFrame)})
 }
-
+ 
+ 
 # Run the app
 shinyApp(ui = ui, server = server)
