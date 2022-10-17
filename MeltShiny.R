1c13cf0dc05a6697a004b1fb12e5353cbdf0db4e
shlokpat6 <shlokpat6@gmail.com>
2022-10-17 17:21:11 -0500

    Fixed the issue with pathlengths not updating properly

--- MeltShiny.R
+++ MeltShiny.R
@@ -4,6 +4,8 @@ library(ggplot2)
 #library(MeltR)
 library(shiny)
 
+counter <- 1
+
 # The UI consists of a navbar page, with a single drop down menu, "File" , which contains a single option "Add data".
 ui <- navbarPage("MeltShiny",
                  navbarMenu("File",
@@ -43,44 +45,43 @@ ui <- navbarPage("MeltShiny",
                                      )
                             )
                  )
-)      
+) 
 
 # Back end
-server <- function(input,output){
+server <- function(input,output, session){
   #Reactive list variable 
-  values <- reactiveValues(masterFrame=NULL,up=NULL)
+  values <- reactiveValues(masterFrame=NULL)
   #Upload Project File
   upload <- observeEvent(eventExpr =input$inputFile,
                          handlerExpr = {
-                           #Declaring variables
+                           # Declaring variables
                            pathlengths <- c(unlist(strsplit(input$pathlengths,",")))
                            req(input$inputFile)
                            fileName = input$inputFile$datapath
                            cd <- read.csv(file = fileName,header=FALSE)
                            df <- cd %>% select_if(~ !any(is.na(.)))
-                           #Creating temporary frame to store sample data
+                           # Creating temporary frame to store sample data
                            columns <- c("Sample", "Pathlength", "Temperature", "Absorbance")
                            tempFrame <- data.frame(matrix(nrow = 0, ncol = 4))
                            colnames(tempFrame) <- columns
                            readings <- ncol(df)
-                           #Loop that appends sample data 
-                           counter <- 1
+                           # Loop that appends sample data 
+                           p <-1
                            for (x in 2:readings){
                              col <- df[x]
                              sample<-rep(c(counter),times=nrow(df[x]))
-                             pathlength<-rep(c(pathlengths[counter]),times=nrow(df[x]))
+                             pathlength<-rep(c(pathlengths[p]),times=nrow(df[x]))
                              col <- df[x]
                              t <- data.frame(sample,pathlength,df[1],df[x])
                              names(t) <- names(tempFrame)
                              tempFrame <- rbind(tempFrame, t)
-                             counter <- counter + 1
+                             p <- p + 1
+                             counter <<- counter + 1
                            }
                            values$numReadings <- counter-1
                            values$masterFrame <- rbind (values$masterFrame, tempFrame)
-                           values$up <- 1
                          }
   )
-  
   output$Table <- renderTable({
     return(values$masterFrame)})
 }
