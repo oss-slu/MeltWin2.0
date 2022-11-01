544f60b7c243b7a3aa1597eb6a50c88351dafa06
Colleen Wade <colleen.wade@slu.edu>
2022-11-01 12:51:48 -0500

    added new panel

--- MeltShiny.R
+++ MeltShiny.R
@@ -48,22 +48,20 @@ ui <- navbarPage(title = "MeltShiny",id = "navbar",
                                        )
                                      )
                             ),
-                 ),navbarMenu("Plots",NULL),
-                 tabPanel("Data Visualization",
-                   sidebarLayout(
-                     sidebarPanel(
-                       width = 3,
-                       h2(align = "center","Data Visualization"),
-                       strong("Check Which Plots to View"),
-                       uiOutput("checkboxes"),
-                       actionButton("automate","Automate Fitting Data")
-                     ),mainPanel(
-                       uiOutput("dataVisualContents"),
-                     )
-                   )
-              
-             
-                 )
+                 ),
+                 navbarMenu("Results",
+                            tabPanel("Vant Hoff Plots", 
+                                     fluidPage(
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                         ),
+                                         mainPanel(
+                                           tableOutput("dataVisualContents")
+                                         )
+                                       )
+                                     )
+                            ),
+                 ),navbarMenu("Plots",NULL)
 ) 
 
 # Back end
