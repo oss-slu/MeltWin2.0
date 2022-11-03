9e001527b4f022970ad4235bfae07b19584f44bc
Colleen Wade <colleen.wade@slu.edu>
2022-11-03 15:58:14 -0500

    splitting ui, global varibales, and server into seprate files

--- server.r
+++ server.r
@@ -1,77 +1,9 @@
-library(dplyr, warn.conflicts = FALSE)
-library(ggplot2)
-library(glue)
-library(MeltR)
-library(shiny)
-
-
-
-
-counter <- 1
-start = 1
-# The UI consists of a navbar page, with a single drop down menu, "File" , which contains a single option "Add data".
-
-ui <- navbarPage(title = "MeltShiny",id = "navbar",
-                 navbarMenu("File",
-                            # When the user clicks the "Add Data" tab panel, a fluid page is created below the nav bar.
-                            # This page contains a side bar panel and a main panel.
-                            # The side bar contains the given options a user has when they click one of the options in the nav bar.
-                            # The main panel contains the graphs or tables.
-                            tabPanel("Add Data", 
-                                     fluidPage(
-                                       sidebarLayout(
-                                         sidebarPanel(
-                                           textInput(label = "Enter the Pathlength for each Absorbance Reading(separated by commas)",
-                                                     placeholder = "E.g: 2,5,3,2,...",
-                                                     inputId = "pathlengths"),
-                                           fileInput(label = "Add Data",
-                                                     inputId = "inputFile",
-                                                     multiple = FALSE,
-                                                     accept = ".csv")
-                                         ),
-                                         mainPanel(
-                                           tableOutput("Table")
-                                         )
-                                       )
-                                     )
-                            )
-                 ),
-                 navbarMenu("Help",
-                            tabPanel("Absorbance in MeltR", 
-                                     fluidPage(
-                                       sidebarLayout(
-                                         sidebarPanel(
-                                         ),
-                                         mainPanel(
-                                           tableOutput("Console")
-                                         )
-                                       )
-                                     )
-                            ),
-                 ),navbarMenu("Plots",NULL),
-                 tabPanel("Data Visualization",
-                   sidebarLayout(
-                     sidebarPanel(
-                       width = 3,
-                       h2(align = "center","Data Visualization"),
-                       strong("Check Which Plots to View"),
-                       uiOutput("checkboxes"),
-                       actionButton("automate","Automate Fitting Data")
-                     ),mainPanel(
-                       uiOutput("dataVisualContents"),
-                     )
-                   )
-              
-             
-                 )
-) 
-
 # Back end
 server <- function(input,output, session){
   #Reactive list variable 
   values <- reactiveValues(masterFrame = NULL,numReadings = NULL)
   #Upload Project File
-
+  
   upload <- observeEvent(eventExpr = input$inputFile,
                          handlerExpr = {
                            # Declaring variables
@@ -102,7 +34,7 @@ server <- function(input,output, session){
                            values$masterFrame <- rbind(values$masterFrame, tempFrame)
                            values$uploaded <- 1
                          }
-
+                         
   )
   output$Table <- renderTable({
     return(values$masterFrame)})
@@ -184,7 +116,7 @@ server <- function(input,output, session){
             hr()
           )
         }
-      #odd # of plots
+        #odd # of plots
       }else{
         if (i == values$numReadings) {
           tagList(
@@ -222,7 +154,7 @@ server <- function(input,output, session){
   })
 }
 
- 
+
 
 # Run the app
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
---
+++ ui.R
@@ -0,0 +1,62 @@
+library(dplyr, warn.conflicts = FALSE)
+library(ggplot2)
+library(glue)
+library(MeltR)
+library(shiny)
+
+# The UI consists of a navbar page, with a single drop down menu, "File" , which contains a single option "Add data".
+
+ui <- navbarPage(title = "MeltShiny",id = "navbar",
+                 navbarMenu("File",
+                            # When the user clicks the "Add Data" tab panel, a fluid page is created below the nav bar.
+                            # This page contains a side bar panel and a main panel.
+                            # The side bar contains the given options a user has when they click one of the options in the nav bar.
+                            # The main panel contains the graphs or tables.
+                            tabPanel("Add Data", 
+                                     fluidPage(
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                           textInput(label = "Enter the Pathlength for each Absorbance Reading(separated by commas)",
+                                                     placeholder = "E.g: 2,5,3,2,...",
+                                                     inputId = "pathlengths"),
+                                           fileInput(label = "Add Data",
+                                                     inputId = "inputFile",
+                                                     multiple = FALSE,
+                                                     accept = ".csv")
+                                         ),
+                                         mainPanel(
+                                           tableOutput("Table")
+                                         )
+                                       )
+                                     )
+                            )
+                 ),
+                 navbarMenu("Help",
+                            tabPanel("Absorbance in MeltR", 
+                                     fluidPage(
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                         ),
+                                         mainPanel(
+                                           tableOutput("Console")
+                                         )
+                                       )
+                                     )
+                            ),
+                 ),navbarMenu("Plots",NULL),
+                 tabPanel("Data Visualization",
+                          sidebarLayout(
+                            sidebarPanel(
+                              width = 3,
+                              h2(align = "center","Data Visualization"),
+                              strong("Check Which Plots to View"),
+                              uiOutput("checkboxes"),
+                              actionButton("automate","Automate Fitting Data")
+                            ),mainPanel(
+                              uiOutput("dataVisualContents"),
+                            )
+                          )
+                          
+                          
+                 )
+) 
